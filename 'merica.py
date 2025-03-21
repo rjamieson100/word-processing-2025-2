1a5525d23dcc0bbb81e376e9cd43834c4f173c28
Jaspo07 <jasper.duff@student.tdsb.on.ca>
2025-03-21 10:11:55 -0400

    Create 'merica.py

---
+++ 'merica.py
@@ -0,0 +1,2 @@
+# List all the words that can be spelled using only the 2-letter shortcodes for the 50 American states. (AL, AK, AZ, AR, CA, CO, CT, ...)
+# Example: VANDAL (Virginia, North Dakota, Alabama).
