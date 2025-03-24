a7c7b89e40ca26189b8c7f34fb52859111322274
Jaspo07 <jasper.duff@student.tdsb.on.ca>
2025-03-23 23:12:22 -0400

    Update backwards.py
    added the beginnings to the code involved for backwards

--- backwards.py
+++ backwards.py
@@ -1,2 +1,9 @@
 # List all the words for which the reversed word is also a word, but not a palindrome.
 # Example: STRESSED
+
+def find_reversible():
+    
+    word_set = set(words)
+    result = []
+    
+    for word in words:
