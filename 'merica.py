80beed7e7dc60733babafe5490c7f650e701237a
super-dollar-deluxe <everett.campbell@student.tdsb.on.ca>
2025-03-24 09:31:15 -0400

    Update 'merica.py
    created list of shortcodes

--- 'merica.py
+++ 'merica.py
@@ -1,2 +1,20 @@
 # List all the words that can be spelled using only the 2-letter shortcodes for the 50 American states. (AL, AK, AZ, AR, CA, CO, CT, ...)
 # Example: VANDAL (Virginia, North Dakota, Alabama).
+
+
+def start():
+    list_codes = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT",
+                       "DE", "DC", "FL", "GA", "HI", "ID", "IL",
+                       "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+                       "MA", "MI", "MN", "MS", "MO", "MT", "NE",
+                       "NV", "NH", "NJ", "NM", "NY", "NC", "ND",
+                       "OH", "OK", "OR", "PA", "RI", "SC", "SD",
+                       "TN", "TX", "UT", "VT", "VA", "WA", "WV",
+                       "WI", "WY"]
+    list_words = ["TEST", "VANDAL"]
+    print(list_words[1])
+    for x in range (len(list_words)-1):
+        print(x)
+
+# -----------
+start()
